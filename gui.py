0aa949af95ace0b30390da2fb642582223bc209d
defeater <duck-mail@ya.ru>
2016-11-01 19:53:52 +0700

    improvements in wrapping; also added some parsing staff that should be in the backend

--- gui.py
+++ gui.py
@@ -5,13 +5,31 @@ root = Tk()
 scrollbar = Scrollbar(root)
 scrollbar.pack(side=RIGHT, fill=Y)
 listbox = Listbox(root, yscrollcommand=scrollbar.set)
-scrollbar.config(command=listbox.yview)
 
-def insert_in_listbox(str):
-    listbox.insert(END, str)
-    listbox.insert(END, '\n')
+t = Text(root, wrap=WORD)
+
+def parse_reply(post):
+    substring = post[:2]
+    if substring == ">>":
+        for i in range (2, len(post[2:])):
+            if not post[i].isdigit():
+                break
+            substring += str(post[i])
+            return substring
+    else:
+        return None
+
+def insert_in_listbox(post):
+    assert(post != None), "Parse string error!"
+
+    reply = parse_reply(post)
+    if reply != None:
+        pass
+        #colorize_reply(str)
+    t.insert(END, post)
+    t.insert(END, '\n')
 
 def on_finish():
-    listbox.pack(side=LEFT, fill=BOTH, expand=TRUE)
-    scrollbar.config(command=listbox.yview)
+    scrollbar.pack(side="right", fill="y", expand=False)
+    t.pack(side="left", fill="both", expand=True)
     root.mainloop()
